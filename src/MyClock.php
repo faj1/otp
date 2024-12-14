8f1f884a77b937f3f3f78205a0aa6f9a3f9ac83d
www <222@qq.com>
2024-12-14 21:40:29 +0700

    初始化

--- src/MyClock.php
+++ src/MyClock.php
@@ -1,8 +1,23 @@
 <?php
-
+declare(strict_types=1);
 namespace Faj1\Otp;
 
-class MyClock
-{
 
+
+
+
+use DateTimeImmutable;
+use Psr\Clock\ClockInterface;
+
+class MyClock implements ClockInterface
+{
+    /**
+     * 返回当前时间作为 DateTimeImmutable 对象。
+     *
+     * @return DateTimeImmutable
+     */
+    public function now(): DateTimeImmutable
+    {
+        return new DateTimeImmutable('now'); // 获取当前时间
+    }
 }
--- src/Otp.php
+++ src/Otp.php
@@ -2,17 +2,35 @@
 
 namespace Faj1\Otp;
 
+
+use OTPHP\TOTP;
+
 class Otp
 {
 
-    public function getSecret()
+    public static function getSecret()
     {
+        $clock = new MyClock(); // Your own implementation of a PSR-20 Clock
+        $otp = TOTP::generate($clock);
+        echo "The OTP secret is: {$otp->getSecret()}\n";
 
     }
 
-    public function getSecret()
+    public static function getOpt()
     {
+        $clock = new MyClock(); // Your own implementation of a PSR-20 Clock
+    }
 
+
+    public static function getImg()
+    {
+        $clock = new MyClock(); // Your own implementation of a PSR-20 Clock
+    }
+
+
+    public static function verify()
+    {
+        $clock = new MyClock(); // Your own implementation of a PSR-20 Clock
     }
 
 
